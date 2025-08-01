f4d704aa0213d31bca4184ff9b4d654780688629
takbirgazi <takbirgazibd@gmail.com>
2025-08-01 17:37:54 +0600

    Done

--- src/app/modules/wallet/wallet.controller.ts
+++ src/app/modules/wallet/wallet.controller.ts
@@ -33,7 +33,37 @@ const cashOut = catchAsync(async (req: Request, res: Response) => {
     })
 });
 
+const cashIn = catchAsync(async (req: Request, res: Response) => {
+    const payload = req.body;
+    const userEmail = req.params.email;
+    const decodedToken = req.user as JwtPayload;
+
+    const user = await WalletService.cashIn(userEmail, payload, decodedToken);
+    sendResponse(res, {
+        statusCode: statusCode.OK,
+        success: true,
+        message: "Information Update Successfully!",
+        data: user
+    })
+});
+
+const sendMoney = catchAsync(async (req: Request, res: Response) => {
+    const payload = req.body;
+    const userEmail = req.params.email;
+    const decodedToken = req.user as JwtPayload;
+
+    const user = await WalletService.sendMoney(userEmail, payload, decodedToken);
+    sendResponse(res, {
+        statusCode: statusCode.OK,
+        success: true,
+        message: "Information Update Successfully!",
+        data: user
+    })
+});
+
 export const WalletController = {
     updateWallet,
     cashOut,
+    cashIn,
+    sendMoney
 }
--- src/app/modules/wallet/wallet.service.ts
+++ src/app/modules/wallet/wallet.service.ts
@@ -128,7 +128,7 @@ const cashOut = async (userEmail: string, payload: IPayload, decodedToken: JwtPa
             fee: (payload.balance as number) * 1.8 / 100,
         }], { session });
 
-        // Create Transaction fro To
+        // Create Transaction for To
         await Transaction.create([{
             user: ifSendUserExist._id,
             amount: payload.balance,
@@ -138,9 +138,237 @@ const cashOut = async (userEmail: string, payload: IPayload, decodedToken: JwtPa
             commission: (payload.balance as number) * 0.4 / 100,
         }], { session });
 
+        const updateUser = await User.findById(decodedToken.userId).session(session).select("-password").populate({
+            path: "wallet",
+            select: "balance status"
+        });
+
         await session.commitTransaction();
         session.endSession();
-        return ifUserExist;
+        return updateUser;
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        throw error;
+    }
+};
+
+const cashIn = async (userEmail: string, payload: IPayload, decodedToken: JwtPayload) => {
+    const session = await Wallet.startSession();
+    session.startTransaction()
+    try {
+        if (payload.status) {
+            throw new AppError(statusCode.NOT_FOUND, "You can't Update User Status");
+        }
+        if (payload.balance) {
+            if (payload.balance < 1) {
+                throw new AppError(statusCode.NOT_FOUND, "Wrong Information Collected!");
+            }
+        }
+
+        const ifUserExist = await User.findById(decodedToken.userId).session(session).select("-password").populate({
+            path: "wallet",
+            select: "balance status"
+        });
+        if (!ifUserExist) {
+            throw new AppError(statusCode.NOT_FOUND, "Agent Not Found!");
+        }
+        if (ifUserExist) {
+            if (ifUserExist.role !== Role.AGENT) {
+                throw new AppError(statusCode.BAD_REQUEST, "You can not make cash in");
+            }
+
+            const userWallet = await Wallet.findById(ifUserExist.wallet).session(session);
+            if (userWallet?.status !== WalletStatus.ACTIVE) {
+                throw new AppError(statusCode.NOT_FOUND, "Agent Account is Blocked");
+            }
+            if (userWallet?.balance && payload?.balance) {
+                const { balance } = payload;
+                if (userWallet.balance < 0 || userWallet.balance < balance) {
+                    throw new AppError(statusCode.BAD_REQUEST, "Insufficient Balance!");
+                }
+                if ((userWallet.balance - balance) < 0) {
+                    throw new AppError(statusCode.BAD_REQUEST, "Something Went Wrong!");
+                }
+                await Wallet.findByIdAndUpdate(userWallet._id, { balance: (userWallet?.balance - balance) }, { new: true, runValidators: true, session });
+            }
+        }
+
+        const ifSendUserExist = await User.findOne({ email: userEmail }).session(session);
+        if (!ifSendUserExist) {
+            throw new AppError(statusCode.NOT_FOUND, "Sender Not Found!");
+        }
+
+        // Send Balance Handle
+        if (ifSendUserExist) {
+            if (ifSendUserExist.role !== Role.USER) {
+                throw new AppError(statusCode.BAD_REQUEST, "Sender not user");
+            }
+            if (!ifSendUserExist.isVerified) {
+                throw new AppError(statusCode.NOT_FOUND, "Sender is Not Verified!");
+            }
+            if (ifSendUserExist.isActive !== IsActive.ACTIVE) {
+                throw new AppError(statusCode.NOT_FOUND, "Sender is Not Active!");
+            }
+
+            const sendUserWallet = await Wallet.findById(ifSendUserExist.wallet).session(session);
+            if (sendUserWallet?.status !== WalletStatus.ACTIVE) {
+                throw new AppError(statusCode.NOT_FOUND, "Sender Account is Blocked");
+            }
+
+            if (sendUserWallet?.balance && payload?.balance) {
+                const { balance } = payload;
+                if (sendUserWallet.balance < 0) {
+                    throw new AppError(statusCode.BAD_REQUEST, "Something Went Wrong!");
+                }
+
+                await Wallet.findByIdAndUpdate(sendUserWallet._id, { balance: (sendUserWallet?.balance + balance) }, { new: true, runValidators: true, session });
+            }
+        }
+
+        // Create Transaction for From
+        await Transaction.create([{
+            user: ifUserExist?._id,
+            amount: payload.balance,
+            type: TransactionType.CASH_IN,
+            transactionId: getTransactionId(),
+            transactionWith: ifSendUserExist._id,
+            fee: 0,
+        }], { session });
+
+        // Create Transaction for To
+        await Transaction.create([{
+            user: ifSendUserExist._id,
+            amount: payload.balance,
+            type: TransactionType.CASH_IN,
+            transactionId: getTransactionId(),
+            transactionWith: ifUserExist?._id,
+            commission: 0,
+        }], { session });
+
+        const updateUser = await User.findById(decodedToken.userId).session(session).select("-password").populate({
+            path: "wallet",
+            select: "balance status"
+        });
+
+        await session.commitTransaction();
+        session.endSession();
+
+        return updateUser;
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        throw error;
+    }
+};
+
+const sendMoney = async (userEmail: string, payload: IPayload, decodedToken: JwtPayload) => {
+    const session = await Wallet.startSession();
+    session.startTransaction()
+    try {
+        if (payload.status) {
+            throw new AppError(statusCode.NOT_FOUND, "You can't Update User Status");
+        }
+        if (payload.balance) {
+            if (payload.balance < 1) {
+                throw new AppError(statusCode.NOT_FOUND, "Wrong Information Collected!");
+            }
+        }
+
+        const ifUserExist = await User.findById(decodedToken.userId).session(session).select("-password").populate({
+            path: "wallet",
+            select: "balance status"
+        });
+        if (!ifUserExist) {
+            throw new AppError(statusCode.NOT_FOUND, "User Not Found!");
+        }
+        if (ifUserExist) {
+            if (ifUserExist.role !== Role.USER) {
+                throw new AppError(statusCode.BAD_REQUEST, "You can not make cash in");
+            }
+
+            const userWallet = await Wallet.findById(ifUserExist.wallet).session(session);
+            if (userWallet?.status !== WalletStatus.ACTIVE) {
+                throw new AppError(statusCode.NOT_FOUND, "User Account is Blocked");
+            }
+            if (userWallet?.balance && payload?.balance) {
+                const { balance } = payload;
+                const balanceWithFee = balance + (balance * 0.5 / 100);
+                if (userWallet.balance < 0 || userWallet.balance < balanceWithFee) {
+                    throw new AppError(statusCode.BAD_REQUEST, "Insufficient Balance!");
+                }
+                if ((userWallet.balance - balanceWithFee) < 0) {
+                    throw new AppError(statusCode.BAD_REQUEST, "Something Went Wrong!");
+                }
+                await Wallet.findByIdAndUpdate(userWallet._id, { balance: (userWallet?.balance - balanceWithFee) }, { new: true, runValidators: true, session });
+            }
+        }
+
+        const ifSendUserExist = await User.findOne({ email: userEmail }).session(session);
+        if (!ifSendUserExist) {
+            throw new AppError(statusCode.NOT_FOUND, "Sender Not Found!");
+        }
+        // If User send to own
+        if (ifUserExist._id.toString() === ifSendUserExist._id.toString()) {
+            throw new AppError(statusCode.BAD_REQUEST, "User and Sender Are Same!");
+        }
+
+        // Send Balance Handle
+        if (ifSendUserExist) {
+            if (ifSendUserExist.role !== Role.USER) {
+                throw new AppError(statusCode.BAD_REQUEST, "Sender not user");
+            }
+            if (!ifSendUserExist.isVerified) {
+                throw new AppError(statusCode.NOT_FOUND, "Sender is Not Verified!");
+            }
+            if (ifSendUserExist.isActive !== IsActive.ACTIVE) {
+                throw new AppError(statusCode.NOT_FOUND, "Sender is Not Active!");
+            }
+
+            const sendUserWallet = await Wallet.findById(ifSendUserExist.wallet).session(session);
+            if (sendUserWallet?.status !== WalletStatus.ACTIVE) {
+                throw new AppError(statusCode.NOT_FOUND, "Sender Account is Blocked");
+            }
+
+            if (sendUserWallet?.balance && payload?.balance) {
+                const { balance } = payload;
+                if (sendUserWallet.balance < 0) {
+                    throw new AppError(statusCode.BAD_REQUEST, "Something Went Wrong!");
+                }
+
+                await Wallet.findByIdAndUpdate(sendUserWallet._id, { balance: (sendUserWallet?.balance + balance) }, { new: true, runValidators: true, session });
+            }
+        }
+
+        // Create Transaction for From
+        await Transaction.create([{
+            user: ifUserExist?._id,
+            amount: payload.balance,
+            type: TransactionType.CASH_IN,
+            transactionId: getTransactionId(),
+            transactionWith: ifSendUserExist._id,
+            fee: (payload.balance as number) * 0.5 / 100,
+        }], { session });
+
+        // Create Transaction for To
+        await Transaction.create([{
+            user: ifSendUserExist._id,
+            amount: payload.balance,
+            type: TransactionType.CASH_IN,
+            transactionId: getTransactionId(),
+            transactionWith: ifUserExist?._id,
+            fee: 0,
+        }], { session });
+
+        const updateUser = await User.findById(decodedToken.userId).session(session).select("-password").populate({
+            path: "wallet",
+            select: "balance status"
+        });
+
+        await session.commitTransaction();
+        session.endSession();
+
+        return updateUser;
     } catch (error) {
         await session.abortTransaction();
         session.endSession();
@@ -151,4 +379,6 @@ const cashOut = async (userEmail: string, payload: IPayload, decodedToken: JwtPa
 export const WalletService = {
     updateWallet,
     cashOut,
+    cashIn,
+    sendMoney
 }
